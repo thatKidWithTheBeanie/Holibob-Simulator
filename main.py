f6d2a3d6cd2398a4711378b91fcee860cf10885c
Booth <iheartmybeanie@gmail.com>
2019-12-05 16:02:11 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,52 @@
 ### NTS: only input in lower case
 ### TD: .lower()
+### NTS: planes go two ways
+'''
+  ___               _     _                                                       
+ | _ )  ___   ___  | |_  | |_  '  ___    ___   _ _    ___    __ __ __  __ _   _  _ 
+ | _ \ / _ \ / _ \ |  _| | ' \  (_-<   / _ \ | ' \  / -_)   \ V  V / / _` | | || |
+ |___/ \___/ \___/ _\__| |_||_| /__/   \___/ |_||_| \___|    \_/\_/  \__,_|  \_, |
+ | |_    ___  | | (_)  __| |  __ _   _  _   ___ | |                          |__/ 
+ | ' \  / _ \ | | | | / _` | / _` | | || | (_-< |_|                               
+ |_||_| \___/ |_| |_| \__,_| \__,_|  \_, | /__/ (_)                               
+                                     |__/                                         
+
+'''
 import random
+
+def gap():
+    print ("")
+    print ("")
+    
 hotels = []
 class Hotel():
-    def __init__(self, hotelName, city, unitsAvai, authNum=None):
+    def __init__(self, hotelName, city, unitsAvai):
         self.hotelName = hotelName
         self.city = city
         self.unitsAvai = unitsAvai
-        if authNum is None:
-            authNum = random.randint(0,100)
-        hotels.append([hotelName, city, authNum])
+        hotels.append([hotelName, city])
 
-    def book():
-        if unitsAvai > 0:
-            unitsAvai - 1
-            print("You have booked this hotel")
 flights = []
 class Flight():
-    def __init__(self, flightCode, fromCity, toCity, dayOfWeek, leaveTime, authNum=None):
+    def __init__(self, flightCode, fromCity, toCity, dayOfWeek, leaveTime):
         self.flightCode = flightCode
         self.fromCity = fromCity
         self.toCity = toCity
         self.dayOfWeek = dayOfWeek
         self.leaveTime = leaveTime
+        flights.append([flightCode, fromCity, toCity, dayOfWeek, leaveTime])
+        
+bookings = []
+class Booking():
+    def __init__(self, bookingCode, username, flightCode, hotelName, authNum=None):
+        self.bookingCode = bookingCode
+        self.username = username
+        self.hotelName = hotelName
+        self.flightCode = flightCode
         ### Random int selected for when user makes/cancels bookings
         ### Not proper practice but i cba to do it and no one will be cracking this
-        if authNum is None:
-            authNum = random.randint(0,100)
-        flights.append([flightCode, fromCity, toCity, authNum])
-        
+        self.authNum = random.randint(100,1000)
+        flights.append([bookingCode, username, flightCode, hotelName, authNum])
 
 ### Static variables
 ### Hotel name, city, available rooms
@@ -44,9 +62,19 @@ MH5230 = Flight("MH5230", "london", "lisbon", 1, 05.00)
 MH5231 = Flight("MH5231", "lisbon", "london", 6, 20.00)
 MH5232 = Flight("MH5232", "london", "paris", 4, 07.30)
 
-#### begin actual code
 
+### Test Booking
+giovanni1234 = Booking("giovanniMH5231", "giovanni", "MH5231", "Lisbon Travel Lodge")
+
+
+###  ___               _   
+### | _ )  ___   ___  | |__
+### | _ \ / _ \ / _ \ | / /
+### |___/ \___/ \___/ |_\_\
+
+### Design the trip                    
 ### parse in home and dest in function below
+
 def FlightsFromPlaceToPlace(home, dest):
     for i in range(len(flights)):
         if home in flights[i][1] and dest in flights[i][2]:
@@ -68,9 +96,56 @@ def designAtrip():
     dest = input ("")
     print ("And where are you travelling from?")
     home =  input ("")
-    print ("")
+    gap()
     TripDesignFacade(dest, home)
 
-designAtrip()
+### Book the trip
+def bookAtrip():
+    gap()
+    print ("You are about to book a holiday, if you would like to be reminded of your options please enter 'remind', if not press return")
+    usrin = input("")
+    if usrin.lower() == "remind":
+        designAtrip()
+    x = 1
+    while x == 1:
+        print ("Please enter your desired flight number")
+        chsnFlight = input("")
+        for i in range(len(flights)):
+            if chsnFlight in flights[i]:
+                print ("You have selected the flight:")
+                print (chsnFlight)
+                x = 2
+    while x == 2:
+        print ("Please enter your desired Hotel")
+        chsnHotel = input("")
+        for i in range(len(hotels)):
+            if chsnHotel in hotels[i]:
+                print ("You have selected the hotel:")
+                print (chsnHotel)
+                x = 3
+    print ("Please enter your first name")
+    usrName = input("")
+    print ("Booking the flight ", chsnFlight)
+    print ("Booking the hotel ", chsnHotel)
+    bookingCodeP = usrName + chsnFlight
+    test = bookingCodeP
+    test = Booking(bookingCodeP, usrName, chsnFlight, chsnHotel)
+    gap()
+    print ("booking code", test.bookingCode)
+    print ("name ", test.username)
+    print ("Flight ", test.flightCode)
+    print ("Hotel name ", test.hotelName)
+    print ("auth number ", test.authNum)
+    
+    
+    
 
-bookATrip()
+
+
+###  __  __          _        
+### |  \/  |  __ _  (_)  _ _  
+### | |\/| | / _` | | | | ' \ 
+### |_|  |_| \__,_| |_| |_||_|
+                          
+designAtrip()
+bookAtrip()
